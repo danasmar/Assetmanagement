c99c3e6625510e9bae53bc540f346772c0a72f0c
danasmar <danasmar@gmail.com>
2026-04-07 09:31:38 +0300

    Add MarketInsights component to InvestorApp

--- src/components/investor/InvestorApp.js
+++ src/components/investor/InvestorApp.js
@@ -7,26 +7,28 @@ import InvestorReports from "./InvestorReports";
 import InvestorDistributions from "./InvestorDistributions";
 import InvestorMessages from "./InvestorMessages";
 import InvestorProfile from "./InvestorProfile";
+import MarketInsights from "./MarketInsights";
 
 export default function InvestorApp({ session, onLogout }) {
- const [page, setPage] = useState('dashboard');
+  const [page, setPage] = useState('dashboard');
 
- const renderPage = () => {
-   switch(page) {
-     case 'dashboard':     return <InvestorDashboard session={session} onPage={setPage} />;
-     case 'portfolio':     return <InvestorPortfolio session={session} />;
-     case 'opportunities': return <InvestorOpportunities session={session} />;
-     case 'reports':       return <InvestorReports session={session} />;
-     case 'distributions': return <InvestorDistributions session={session} />;
-     case 'messages':      return <InvestorMessages session={session} />;
-     case 'profile':       return <InvestorProfile session={session} onLogout={onLogout} />;
-     default:              return <InvestorDashboard session={session} onPage={setPage} />;
-   }
- };
+  const renderPage = () => {
+    switch(page) {
+      case 'dashboard':     return <InvestorDashboard session={session} onPage={setPage} />;
+      case 'market':        return <MarketInsights session={session} />;
+      case 'portfolio':     return <InvestorPortfolio session={session} />;
+      case 'opportunities': return <InvestorOpportunities session={session} />;
+      case 'reports':       return <InvestorReports session={session} />;
+      case 'distributions': return <InvestorDistributions session={session} />;
+      case 'messages':      return <InvestorMessages session={session} />;
+      case 'profile':       return <InvestorProfile session={session} />;
+      default:              return <InvestorDashboard session={session} onPage={setPage} />;
+    }
+  };
 
- return (
-   <Layout page={page} onPageChange={setPage} session={session} onLogout={onLogout} navItems={INVESTOR_NAV}>
-     <div style={{ padding:'1rem 0.75rem' }}>{renderPage()}</div>
-   </Layout>
- );
+  return (
+    <Layout nav={INVESTOR_NAV} activePage={page} onNavigate={setPage} onLogout={onLogout} session={session}>
+      {renderPage()}
+    </Layout>
+  );
 }
